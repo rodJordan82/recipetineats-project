9ef409f28db94a74897ffc092410cce286b24b1f
CapSap <cshotam@gmail.com>
2022-05-04 11:09:35 +1000

    basic header for page done

---
+++ recipetineats/src/containers/home/Home.jsx
@@ -0,0 +1,110 @@
+import styles from "./home.module.scss";
+
+const Home = () => {
+  return (
+    <div className={styles.home_container}>
+      <a
+        href="https://www.recipetineats.com/"
+        className={styles.home_container__heading}
+      >
+        RecipeTin Eats
+      </a>
+      <div className={styles.home_container__search}>
+        <input
+          className={styles.home_container__search_input}
+          type="text"
+          placeholder="Search Receipes..."
+        ></input>
+        <div className={styles.home_container__search_socialLinks}>
+          <a
+            className={styles.social_icon}
+            href="http://instagram.com/recipe_tin"
+            tabindex="0"
+          >
+            <svg
+              className={styles.social_icon__svg}
+              aria-hidden="true"
+              data-prefix="fab"
+              data-icon="instagram"
+              role="img"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 448 512"
+              data-fa-i2svg=""
+            >
+              <path
+                fill="currentColor"
+                d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"
+              ></path>
+            </svg>
+          </a>
+
+          <a
+            className={styles.social_icon}
+            href="https://www.facebook.com/RecipeTinEats"
+            tabindex="0"
+          >
+            <svg
+              className={styles.social_icon__svg}
+              aria-hidden="true"
+              data-prefix="fab"
+              data-icon="facebook-f"
+              role="img"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 264 512"
+              data-fa-i2svg=""
+            >
+              <path
+                fill="currentColor"
+                d="M76.7 512V283H0v-91h76.7v-71.7C76.7 42.4 124.3 0 193.8 0c33.3 0 61.9 2.5 70.2 3.6V85h-48.2c-37.8 0-45.1 18-45.1 44.3V192H256l-11.7 91h-73.6v229"
+              ></path>
+            </svg>
+          </a>
+          <a
+            className={styles.social_icon}
+            href="http://www.pinterest.com/recipetin/"
+            tabindex="0"
+          >
+            <svg
+              className={styles.social_icon__svg}
+              aria-hidden="true"
+              data-prefix="fab"
+              data-icon="pinterest-p"
+              role="img"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 384 512"
+              data-fa-i2svg=""
+            >
+              <path
+                fill="currentColor"
+                d="M204 6.5C101.4 6.5 0 74.9 0 185.6 0 256 39.6 296 63.6 296c9.9 0 15.6-27.6 15.6-35.4 0-9.3-23.7-29.1-23.7-67.8 0-80.4 61.2-137.4 140.4-137.4 68.1 0 118.5 38.7 118.5 109.8 0 53.1-21.3 152.7-90.3 152.7-24.9 0-46.2-18-46.2-43.8 0-37.8 26.4-74.4 26.4-113.4 0-66.2-93.9-54.2-93.9 25.8 0 16.8 2.1 35.4 9.6 50.7-13.8 59.4-42 147.9-42 209.1 0 18.9 2.7 37.5 4.5 56.4 3.4 3.8 1.7 3.4 6.9 1.5 50.4-69 48.6-82.5 71.4-172.8 12.3 23.4 44.1 36 69.3 36 106.2 0 153.9-103.5 153.9-196.8C384 71.3 298.2 6.5 204 6.5z"
+              ></path>
+            </svg>
+          </a>
+          <a
+            className={styles.social_icon}
+            href="https://www.youtube.com/c/recipetineats"
+            tabindex="0"
+          >
+            <svg
+              className={styles.social_icon__svg}
+              aria-hidden="true"
+              data-prefix="fab"
+              data-icon="youtube"
+              role="img"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 576 512"
+              data-fa-i2svg=""
+            >
+              <path
+                fill="currentColor"
+                d="M549.655 124.083c-6.281-23.65-24.787-42.276-48.284-48.597C458.781 64 288 64 288 64S117.22 64 74.629 75.486c-23.497 6.322-42.003 24.947-48.284 48.597-11.412 42.867-11.412 132.305-11.412 132.305s0 89.438 11.412 132.305c6.281 23.65 24.787 41.5 48.284 47.821C117.22 448 288 448 288 448s170.78 0 213.371-11.486c23.497-6.321 42.003-24.171 48.284-47.821 11.412-42.867 11.412-132.305 11.412-132.305s0-89.438-11.412-132.305zm-317.51 213.508V175.185l142.739 81.205-142.739 81.201z"
+              ></path>
+            </svg>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
