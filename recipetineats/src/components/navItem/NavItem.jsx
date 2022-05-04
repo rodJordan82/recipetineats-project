3183b49145c9b0c737e41eb2a0f32f72634045a9
CapSap <cshotam@gmail.com>
2022-05-04 13:36:26 +1000

    wip- submenu commit. not finished

--- recipetineats/src/components/navItem/NavItem.jsx
+++ recipetineats/src/components/navItem/NavItem.jsx
@@ -1,6 +1,7 @@
 import styles from "./NavItem.module.scss";
 
-const NavItem = (props) => {
+const NavItem = ({ props }) => {
+  console.log(props.subMenu);
   return (
     <li className={styles.nav}>
       <a className={styles.nav__link} href="/#">
